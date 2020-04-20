a930e319286b77c71dc002abda0abf9694ba1bae
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-20 18:20:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,7 @@
-["snowball", "snowman", "hot chocolate"].concat(["jacket"])
+def using_concat([array1], [array2])
+  array1 = ["snowball", "snowman", "hot chocolate"]
+  array2 = ["jacket"])
+  puts array1.concat(array2)
+end
  
   
